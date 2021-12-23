f1fa106b9949faad29ca3b2ca3a7f8fca4a0222b
Venkat <Venkat@Venkats-MacBook-Pro.local>
2021-12-23 10:41:18 -0500

    auth changes

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "cloudtest252f1b31": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
